00ebbd32ac181d3125c30570dae49920643eb5c3
njoel24 <cristiano.anselmi@gmail.com>
2017-04-23 22:09:48 +0200

    epic(game): add chooseCompagno component

---
+++ src/components/ChooseCompagno.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { startMatch, setWinner,  endTurn,
+		changeTurn, play, playBot} from '../actions/match'
+import { playAuctionBot, 
+	 playAuction,
+	 endAuction, 
+	 exitAuction,
+	 changeTurnAuction, chooseCompagno } from '../actions/auction'
+
+class ChooseCompagno extends React.Component {
+
+    createSelectItems() {
+     let items = [];         
+     for (var key in this.props.allCards) {
+        if(this.props.allCards.hasOwnProperty(key)){
+          items.push(<option key={key} value={key}>{this.props.allCards[key].nome + " " + this.props.allCards[key].seme}</option>);   
+        }
+     }
+     return items;
+    }  
+
+	render () {	
+		if(this.props.show) {	
+		return (
+			<div>
+                <select id="compagno">
+                    {this.createSelectItems()}
+                </select>
+				<button onClick={ this.props.chooseCompagno}>Ok</button>
+			</div>
+		)
+		} else {
+			return null
+		}
+	}
+}  
+
+const mapStateToProps = function(store) {
+  return {
+      allCards: state.cards
+  };
+}
+
+const mapDispatchToProps = function(dispatch, ownProps) {
+  return {
+    chooseCompagno: () => {
+			let value= 0
+			if(document.getElementById("compagno")){
+				value = document.getElementById("compagno").value
+			}
+      dispatch(chooseCompagno(value));
+    }	
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChooseCompagno);
